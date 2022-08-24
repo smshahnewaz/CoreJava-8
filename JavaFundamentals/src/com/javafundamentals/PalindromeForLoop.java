45a6ca4f047aa3927434b29d51e99e404c6680bd
SM Shahnewaz <shahnewazsm@gmail.com>
2022-08-23 23:55:59 -0400

    commit

--- JavaFundamentals/src/com/javafundamentals/PalindromeForLoop.java
+++ JavaFundamentals/src/com/javafundamentals/PalindromeForLoop.java
@@ -1,21 +1,14 @@
 package com.javafundamentals;
 import java.util.*;
 
-public class Palindrome {
+public class PalindromeForLoop {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		String org;
-		String rev = "";
-		System.out.print("Enter a String   : ");
-		org = sc.next();
-		int length = org.length();
-		for(int i=length-1; i>=0; i--) {
-			rev=rev+org.charAt(i);
-		}
-		if(org.equals(rev)) {
-				System.out.printf("%s is a Palindrome",org);
-		}else {
-				System.out.printf("%s is a Palindrome",org);
-		}
+		  String org; String rev = ""; System.out.print("Enter a String   : "); org =
+		  sc.next(); int length = org.length(); for(int i=length-1; i>=0; i--) {
+		  rev+=org.charAt(i); } if(org.equals(rev)) {
+		  System.out.printf("%s is a Palindrome!",org); }else if(org!=rev) {
+		  System.out.printf("%s is NOT a Palindrome!",org); 
+		  }
 		}
 	}
