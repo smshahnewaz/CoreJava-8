45c633477681a465c260b3c0752527f993e1f643
SM Shahnewaz <shahnewazsm@gmail.com>
2022-08-23 23:02:37 -0400

    commit

--- JavaFundamentals/src/com/javafundamentals/DescendignLoop.java
+++ JavaFundamentals/src/com/javafundamentals/DescendignLoop.java
@@ -3,11 +3,17 @@
 package com.javafundamentals;
 
 public class DescendignLoop {
-	
+		// main method of the class 
 	 public static void main(String[] args) {
+		 // Driver Code 
+		 System.out.println("=====Looping 100 to 0 backward====");
 		 for(int i =100; i>=0; i=i-5) {
 				System.out.print(i+" ");
 			}
+		 
+		 System.out.println("\n\n=====Looping 0 to 100 onward====");
+		 for(int i=0; i<=100;i+=5) {
+			 System.out.print(i+" ");
+		 }
 	}
-
 }
